eb13a5817a3effe214cabcd52e65886511f54d1c
Mohammed Mostakim Ornob <mohammed.ornob@cint.com>
2023-08-04 11:03:03 +0200

    Add event consumer and api projects

---
+++ DemoService.Api/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoService.Api.Controllers;
+
+[ApiController]
+[Route("test")]
+public class TestController : ControllerBase
+{
+    // GET
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
